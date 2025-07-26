8f5bb21066103766b5e35dbc6aa643bddaf8ae90
AnanoMakharashvili <ananomakharashvili@gmail.com>
2025-07-26 22:50:11 +0300

    fix: add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/faq-accordion-card-main/",
+  base: "/faq-accordion-card-main/", // GitHub repo-ს სახელი
 });
